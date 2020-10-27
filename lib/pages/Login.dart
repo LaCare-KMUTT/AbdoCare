b31848e5e6f8f6af3e7239681e7ac8b5282e4eb7
Pornpimon <pornpimon@Pornpimons-MacBook-Pro.local>
2020-10-27 14:44:59 +0700

    fix bug

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,183 +1,37 @@
-import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dialogflow/dialogflow_v2.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-// This widget is the home page of your application. It is stateful, meaning
-// that it has a State object (defined below) that contains fields that affect
-// how it looks.
-
-// This class is the configuration for the state. It holds the values (in this
-// case the title) provided by the parent (in this case the App widget) and
-// used by the build method of the State. Fields in a Widget subclass are
-// always marked "final".
-
-  final String title;
-
+class LoginPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  void response(query) async {
-    AuthGoogle authGoogle =
-        await AuthGoogle(fileJson: "assets/abdo-bot-briv-9ed7278f51f5.json")
-            .build();
-    Dialogflow dialogflow =
-        Dialogflow(authGoogle: authGoogle, language: Language.thai);
-    AIResponse aiResponse = await dialogflow.detectIntent(query);
-    setState(() {
-      messsages.insert(0, {
-        "data": 0,
-        "message": aiResponse.getListMessage()[0]["text"]["text"][0].toString()
-      });
-    });
-    //print(aiResponse.getMessage());
-    print(aiResponse.getListMessage()[0]["text"]["text"][0].toString());
-  }
-
-  final messageInsert = TextEditingController();
-  List<Map> messsages = List();
-
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFC37447),
-        title: Text(
-          "Abdo Bot",
-        ),
+        title: Text('AbdoCare'),
       ),
       body: Container(
-        child: Column(
+        child: ListView(
+          shrinkWrap: true,
           children: <Widget>[
-            Flexible(
-                child: ListView.builder(
-                    reverse: true,
-                    itemCount: messsages.length,
-                    itemBuilder: (context, index) => chat(
-                        messsages[index]["message"].toString(),
-                        messsages[index]["data"]))),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              height: 5.0,
-              color: Color(0xFFC37447),
-            ),
-            Container(
-              child: ListTile(
-                title: Container(
-                  height: 35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    color: Color.fromRGBO(220, 220, 220, 1),
-                  ),
-                  padding: EdgeInsets.only(left: 15),
-                  child: TextFormField(
-                    controller: messageInsert,
-                    decoration: InputDecoration(
-                      hintText: "ส่งข้อความ...",
-                      hintStyle: TextStyle(color: Colors.black26),
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                    ),
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                    onChanged: (value) {},
-                  ),
-                ),
-                trailing: IconButton(
-                    icon: Icon(
-                      Icons.send,
-                      size: 30.0,
-                      color: Color(0xFFC37447),
-                    ),
-                    onPressed: () {
-                      if (messageInsert.text.isEmpty) {
-                        print("empty message");
-                      } else {
-                        setState(() {
-                          messsages.insert(
-                              0, {"data": 1, "message": messageInsert.text});
-                        });
-                        response(messageInsert.text);
-                        messageInsert.clear();
-                      }
-                      FocusScopeNode currentFocus = FocusScope.of(context);
-                      if (!currentFocus.hasPrimaryFocus) {
-                        currentFocus.unfocus();
-                      }
-                    }),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+              child: TextField(
+                //ใส่ขนาดว่าจะเป็นขนาดไหน
+                style: Theme.of(context).textTheme.bodyText1,
+                //ทำให้textไปตรงกลางของช่อง
+                textAlign: TextAlign.center,
+                //ใส่ว่าช่องนี้จะใส่อะไรเป็นข้อความแนะนำ
+                decoration: InputDecoration(labelText: 'รหัสโค้ด'),
               ),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  //for better one i have use the bubble package check out the pubspec.yaml
-
-  Widget chat(String message, int data) {
-    return Container(
-      padding: EdgeInsets.only(left: 5, right: 5),
-      child: Row(
-        mainAxisAlignment:
-            data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: [
-          data == 0
-              ? Container(
-                  height: 40,
-                  width: 40,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/robot.jpg"),
-                  ),
-                )
-              : Container(),
-          Padding(
-            padding: EdgeInsets.all(5.0),
-            child: Bubble(
-                radius: Radius.circular(15.0),
-                color: data == 0 ? Color(0xFFF1B43F) : Color(0xFFFCECBC),
-                elevation: 0.0,
-                child: Padding(
-                  padding: EdgeInsets.all(1.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      SizedBox(
-                        width: 5.0,
-                      ),
-                      Flexible(
-                          child: Container(
-                        constraints: BoxConstraints(maxWidth: 200),
-                        child: Text(
-                          message,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ))
-                    ],
-                  ),
-                )),
-          ),
-          data == 1
-              ? Container(
-                  height: 40,
-                  width: 40,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/default.jpg"),
-                  ),
-                )
-              : Container(),
-        ],
       ),
     );
   }
