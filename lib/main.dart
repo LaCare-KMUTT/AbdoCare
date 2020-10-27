ce9cdfbf9d9b0ba43c455b4ff3c3ba10506a0ef6
Pornpimon <pornpimon@Pornpimons-MacBook-Pro.local>
2020-10-27 16:37:45 +0700

    change font

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "Poppins"),
+      theme: ThemeData(fontFamily: "Prompt"),
       home: LoginPage(),
       routes: {
         '/profile_page': (context) => ProfilePage(),
