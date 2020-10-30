f517f2ec895e59703220be3e388916bf2c59e2e7
Warisara <57939595+prawproudd@users.noreply.github.com>
2020-10-30 16:35:19 +0700

    Chatbot (#7)
    * Change function name
    
    * Change function name
    
    * Change pubspec version
    
    * Chatbot
    
    * rename
    
    * fix bug
    
    * fix bug
    
    * add richbar
    
    * add bottom bar
    
    * Rich menu update
    
    Co-authored-by: Pornpimon <pornpimon@Pornpimons-MacBook-Pro.local>
    Co-authored-by: Warisara C <warisara@Warisaras-MacBook-Pro.local>
    Co-authored-by: Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
    Co-authored-by: PrompimonPang <papangdroppaul@gmail.com>
    Co-authored-by: PornpimonPang <57941667+PormpimonPang@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
+import 'pages/Chat.dart';
 import 'pages/Login.dart';
 import 'pages/Profile.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) => MaterialApp(
         theme: ThemeData(fontFamily: "Prompt"),
