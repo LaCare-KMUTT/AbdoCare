9f565379ec8ecc30db8f6d51aecd94666ec3160c
PornpimonPang <57941667+PormpimonPang@users.noreply.github.com>
2020-10-30 15:48:05 +0700

    Login first time and navigate to profile  (#3)
    * First login and profile
    
    * assets image
    
    * upgrade version
    
    * fix bug
    
    * change font size
    
    * add font
    
    * font
    
    * change font
    
    * change style
    
    * Setpin
    
    * fix bug
    
    Co-authored-by: Pornpimon <pornpimon@Pornpimons-MacBook-Pro.local>
    Co-authored-by: Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,20 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+
 import 'pages/Login.dart';
+import 'pages/Profile.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData(fontFamily: "Prompt"),
+        home: LoginPage(),
+        routes: {
+          '/profile_page': (context) => ProfilePage(),
+          '/login_page': (context) => LoginPage(),
+        },
+      );
 }
