b1abfef224d082aaede608bf193ccef27ea39fcf
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2020-11-12 23:50:54 +0700

    try to implement cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
-import * as functions from 'firebase-functions';
-
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as moment from "moment";
+admin.initializeApp(functions.config().firebase);
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,3 +9,13 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const calculateAge = functions.https.onCall((data, context) => {
+  const dobString = data['dob'];
+  const dob = moment(dobString, 'MM-DD-YYYY').toDate();
+  const age = moment().diff(dob, "years");
+  console.log(age);
+  return {
+    age: age,
+  };
+});
