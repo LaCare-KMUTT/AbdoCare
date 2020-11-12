087d3c6272ed8c6d55d950c71856d0e1d3921ba4
PrompimonPang <papangdroppaul@gmail.com>
2020-11-12 17:50:31 +0700

    show pre-op topic and information

---
+++ lib/Widget/pre_op_training.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class PreOpTrainingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _PreOpTrainingPageState();
+  }
+}
+
+class _PreOpTrainingPageState extends State<PreOpTrainingPage> {
+  int number;
+  String topic;
+  String detail;
+  void detailpage(int number, String topic, String detail) {
+    if (number == 0) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => pushToScreen(context, topic, detail)),
+      );
+    } else if (number == 1) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => pushToScreen(context, topic, detail)),
+      );
+    } else if (number == 2) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => pushToScreen(context, topic, detail)),
+      );
+    } else if (number == 3) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => pushToScreen(context, topic, detail)),
+      );
+    }
+  }
+
+  pushToScreen(BuildContext context, String topic, String detail) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(topic),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            detail,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var list = [
+      {
+        'topic': "การเตรียมตัวก่อนผ่าตัด",
+        "id": 0,
+        "detail":
+            "ในช่วง 1 วันก่อนผ่าตัดจะมีการงดน้ำงดอาหาร และจะมีการสวนก้นเพื่อให้ขับถ่ายของเสีย"
+      },
+      {
+        'topic': "การปฎิบัติตัวหลังการผ่าตัด",
+        "id": 1,
+        "detail": "พยายามลุกเดิน"
+      },
+      {
+        'topic': "อาหารที่ควรรับประทาน",
+        "id": 2,
+        "detail": "หลีกเลี่ยงอาหารประเภททอด"
+      }
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('การฝึกสอนและอบรม'),
+      ),
+      body: Container(
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                for (var item in list)
+                  Card(
+                    child: FlatButton(
+                      onPressed: () {
+                        number = item['id'];
+                        topic = item['topic'];
+                        detail = item['detail'];
+                        detailpage(number, topic, detail);
+                        pushToScreen(context, topic, detail);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(item['topic'],
+                                style: Theme.of(context).textTheme.bodyText1),
+                          ),
+                          Icon(
+                            Icons.navigate_next,
+                            size: 32,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
