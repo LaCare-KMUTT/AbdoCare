e1487075aba1ebfe139c3d3d0f854b507a08e24b
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2020-12-22 19:14:50 +0700

    remove unused import

--- lib/pages/set_pin_page.dart
+++ lib/pages/set_pin_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import '../services/firebase_service.dart';
 import '../widget/pin/@enum/pin_mode.dart';
