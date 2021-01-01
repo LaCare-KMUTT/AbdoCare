eb2cb2053e866cd89f5b9a049a3ce0fc279f3d86
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-01-01 14:43:16 +0700

    Fix query in Profile page (#23)
    * fix profile page query
    
    * add wating stage futurebuilder

--- lib/services/interfaces/firebase_service_interface.dart
+++ lib/services/interfaces/firebase_service_interface.dart
@@ -3,16 +3,25 @@ import 'package:flutter/material.dart';
 
 abstract class IFirebaseService {
   Future<bool> login(String uniqueKey);
+
   Future<void> signout();
+
   String getUserId();
+
   Stream<DocumentSnapshot> getCollectionSnapshotByDocId(
       {@required String collection, @required String docId});
+
   Future<String> getAStringValueFormField(
       {@required String collection,
       @required String docId,
       @required String field});
+
   void updateFieldCollection(
       {@required String collection,
       @required String docId,
       @required Map<String, dynamic> updateField});
+
+  Future<Map<String, dynamic>> getLatestAnSubCollection({
+    @required String userId,
+  });
 }
