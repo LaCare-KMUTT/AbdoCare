e22e0a1df5c7f053c145aaaf8f91c49dbc811716
PrompimonPang <papangdroppaul@gmail.com>
2021-01-13 11:24:26 +0700

    gauge chart

--- lib/widget/radial_gauge_chart .dart
+++ lib/widget/radial_gauge_chart .dart
@@ -9,74 +9,76 @@ class PieChart extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SfRadialGauge(
-                enableLoadingAnimation: true,
-                animationDuration: 4000,
-                axes: <RadialAxis>[
-                  RadialAxis(
-                      axisLineStyle: AxisLineStyle(
-                        thickness: 0.4,
-                        thicknessUnit: GaugeSizeUnit.factor,
-                        gradient: SweepGradient(colors: <Color>[
-                          Colors.red,
-                          Colors.orange,
-                          Colors.yellow,
-                          Colors.green
-                        ], stops: <double>[
-                          0.10,
-                          0.25,
-                          0.50,
-                          1
-                        ]),
-                      ),
-                      showTicks: false,
-                      canScaleToFit: true,
-                      startAngle: 180,
-                      endAngle: 0,
-                      interval: 2,
-                      minimum: 0,
-                      maximum: 20,
-                      ranges: <GaugeRange>[
-                        GaugeRange(
-                            startValue: 0,
-                            endValue: 4,
-                            color: Colors.red,
-                            startWidth: 30,
-                            endWidth: 30),
-                        GaugeRange(
-                            startValue: 4,
-                            endValue: 8,
-                            color: Colors.orange,
-                            startWidth: 30,
-                            endWidth: 30),
-                        GaugeRange(
-                            startValue: 8,
-                            endValue: 11,
-                            color: Colors.yellow,
-                            startWidth: 30,
-                            endWidth: 30),
-                        GaugeRange(
-                            startValue: 11,
-                            endValue: 20,
-                            color: Colors.green,
-                            startWidth: 30,
-                            endWidth: 30)
-                      ],
-                      pointers: <GaugePointer>[
-                        NeedlePointer(
-                            value: 12,
-                            lengthUnit: GaugeSizeUnit.logicalPixel,
-                            needleLength: 70)
-                      ],
-                      annotations: <GaugeAnnotation>[
-                        GaugeAnnotation(
-                            widget: Container(
-                                child: Text(
-                              'ผลคะแนน\n12 คะแนน',
-                            )),
-                            angle: 90,
-                            positionFactor: 0.5)
-                      ])
-                ]),
+              enableLoadingAnimation: true,
+              animationDuration: 4000,
+              axes: <RadialAxis>[
+                RadialAxis(
+                    axisLineStyle: AxisLineStyle(
+                      thickness: 50,
+                      /*For gradient color*/
+                      // thicknessUnit: GaugeSizeUnit.factor,
+                      // gradient: SweepGradient(colors: <Color>[
+                      //   Colors.red,
+                      //   Colors.orange,
+                      //   Colors.yellow,
+                      //   Colors.green
+                      // ], stops: <double>[
+                      //   0.10,
+                      //   0.25,
+                      //   0.50,
+                      //   1
+                      // ]),
+                    ),
+                    startAngle: 180,
+                    endAngle: 0,
+                    canScaleToFit: true,
+                    interval: 2,
+                    minimum: 0,
+                    maximum: 20,
+                    showTicks: false,
+                    ranges: <GaugeRange>[
+                      GaugeRange(
+                          startValue: 0,
+                          endValue: 4,
+                          color: Colors.red,
+                          startWidth: 50,
+                          endWidth: 50),
+                      GaugeRange(
+                          startValue: 4,
+                          endValue: 8,
+                          color: Colors.orange,
+                          startWidth: 50,
+                          endWidth: 50),
+                      GaugeRange(
+                          startValue: 8,
+                          endValue: 12,
+                          color: Colors.yellow,
+                          startWidth: 50,
+                          endWidth: 50),
+                      GaugeRange(
+                          startValue: 12,
+                          endValue: 20,
+                          color: Colors.green,
+                          startWidth: 50,
+                          endWidth: 50)
+                    ],
+                    pointers: <GaugePointer>[
+                      NeedlePointer(
+                          value: 12,
+                          lengthUnit: GaugeSizeUnit.logicalPixel,
+                          needleLength: 80)
+                    ],
+                    annotations: <GaugeAnnotation>[
+                      GaugeAnnotation(
+                          widget: Container(
+                              child: Text(
+                            'ผลคะแนน\n12 คะแนน',
+                          )),
+                          angle: 90,
+                          positionFactor: 0.5)
+                    ])
+              ],
+            ),
             Text('การแปลผล',
                 style: Theme.of(context).textTheme.bodyText1,
                 textAlign: TextAlign.left),
