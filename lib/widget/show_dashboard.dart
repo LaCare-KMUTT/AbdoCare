72fa80abc6b1e567e3f913ae8edf1e9e16c97d78
PrompimonPang <papangdroppaul@gmail.com>
2021-01-21 07:46:53 +0700

    query health information

--- lib/widget/show_dashboard.dart
+++ lib/widget/show_dashboard.dart
@@ -1,5 +1,9 @@
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../services/interfaces/calculation_service_interface.dart';
+import '../services/interfaces/firebase_service_interface.dart';
+import '../services/service_locator.dart';
 import 'ADL_chart.dart';
 import 'line_chart.dart';
 import 'radial_gauge_chart .dart';
@@ -19,112 +23,176 @@ class _ShowDashboardState extends State<ShowDashboard> {
     PointSeries(day: 5, point: 4),
     PointSeries(day: 6, point: 2),
   ];
+  final IFirebaseService _firebaseService = locator<IFirebaseService>();
+  final ICalculationService _calculationService =
+      locator<ICalculationService>();
+  var _userId;
+  var _userCollection;
+  @override
+  void initState() {
+    super.initState();
+    var userId = _firebaseService.getUserId();
+    if (userId.isNotEmpty) {
+      print('$userId is here');
+    } else {
+      print('$userId not found');
+    }
+    setState(() {
+      _userId = userId;
+      _userCollection = _firebaseService.getCollectionSnapshotByDocId(
+          collection: 'Users', docId: _userId);
+    });
+  }
 
   @override
-  Widget build(BuildContext context) => Container(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+  Widget build(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+        stream: _userCollection,
+        builder: (context, userCollection) {
+          if (!userCollection.hasData) {
+            return Center(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Container(
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(5, 10, 0, 10),
-                                child: Text(
-                                  'ข้อมูลสุขภาพ',
-                                ),
-                              ),
-                            ],
-                          ),
-                          Text('อายุ 21 ปี',
-                              style: Theme.of(context).textTheme.bodyText1),
-                          Text('ส่วนสูง 160.0 ซม.',
-                              style: Theme.of(context).textTheme.bodyText1),
-                          Text('น้ำหนัก 60.0 กก.',
-                              style: Theme.of(context).textTheme.bodyText1),
-                          Text('ค่าดัชนีมวลกาย 23.44 Kg/m2 (ท้วม)',
-                              style: Theme.of(context).textTheme.bodyText1),
-                          Text('น้ำหนักเมื่อ 3 เดือนที่ผ่านมา 65.0 กก.',
-                              style: Theme.of(context).textTheme.bodyText1),
-                          Text('%BWL = 2%',
-                              style: Theme.of(context).textTheme.bodyText1),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(5, 10, 0, 10),
-                                child: Text(
-                                  'ความสามารถในการปฏิบัติกิจวัตรประจำวัน',
-                                ),
-                              ),
-                            ],
-                          ),
-                          ADLTable(),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(5, 10, 0, 10),
-                                child: Text(
-                                  'ผลการประเมินการปฏิบัติกิจวัตรประจำวัน',
-                                ),
-                              ),
-                            ],
-                          ),
-                          PieChart(),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(5, 10, 0, 10),
-                                child: Text(
-                                  'ความเจ็บปวด',
-                                ),
-                              ),
-                            ],
-                          ),
-                          LineChart(data2: data2),
-                        ],
-                      ),
-                    ),
-                  ),
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Text('Loading...'),
                 ],
               ),
-            ),
-          ],
-        ),
-      );
+            );
+          } else {
+            return FutureBuilder<Map<String, dynamic>>(
+                future:
+                    _firebaseService.getLatestAnSubCollection(userId: _userId),
+                builder: (context, anSubCollection) {
+                  if (!anSubCollection.hasData) {
+                    return Center(
+                      child: Column(
+                        children: <Widget>[
+                          CircularProgressIndicator(),
+                          Text('Loading...'),
+                        ],
+                      ),
+                    );
+                  }
+                  return ListView(
+                    shrinkWrap: true,
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 10, right: 10, top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Container(
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              5, 10, 0, 10),
+                                          child: Text(
+                                            'ข้อมูลสุขภาพ',
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Text(
+                                        '''อายุ${_calculationService.calculateAge(DateFormat('yyyy-mm-dd').parse(userCollection.data['dob']))}ปี''',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1),
+                                    Text(
+                                        '''ส่วนสูง ${anSubCollection.data['height']} ซม.''',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1),
+                                    Text(
+                                        '''น้ำหนัก ${anSubCollection.data['weight']} กก.''',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1),
+                                    Text(
+                                        'ค่าดัชนีมวลกาย ${_calculationService.calculateBMI(anSubCollection.data['weight'], anSubCollection.data['height'])} Kg/m2 ',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1),
+                                    // We haven't calculated %BWL yet.
+                                    Text('%BWL = 2%',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              5, 10, 0, 10),
+                                          child: Text(
+                                            '''ความสามารถในการปฏิบัติกิจวัตรประจำวัน''',
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    ADLTable(),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              5, 10, 0, 10),
+                                          child: Text(
+                                            '''ผลการประเมินการปฏิบัติกิจวัตรประจำวัน''',
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    PieChart(),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Card(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              5, 10, 0, 10),
+                                          child: Text(
+                                            'ความเจ็บปวด',
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    LineChart(data2: data2),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                });
+          }
+        });
+  }
 }
