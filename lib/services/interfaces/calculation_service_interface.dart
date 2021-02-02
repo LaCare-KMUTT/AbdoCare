ae25367500072a106d60b63de6bf7ea79544563d
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-02-02 20:59:58 +0700

    update formatDateToThaiString

--- lib/services/interfaces/calculation_service_interface.dart
+++ lib/services/interfaces/calculation_service_interface.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+
 abstract class ICalculationService {
   int calculateAge(DateTime birthDate);
   String calculateBMI(int weight, int height);
   DateTime formatDate({DateTime date, String dateString});
-  String formatDateToThaiString({DateTime date, String dateString});
+  String formatDateToThaiString(
+      {DateTime date, String dateString, @required bool isBuddhist});
 }
