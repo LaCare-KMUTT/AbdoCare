b5704e2e27ccffd208e303ec4efc4a36e0308034
PrompimonPang <papangdroppaul@gmail.com>
2021-02-02 23:53:11 +0700

    setting screenSize

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,111 +18,114 @@ class _LoginPageState extends State<LoginPage> {
   bool _validateUniqueKey = false;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('AbdoCare'),
-        ),
-        body: Container(
-          child: ListView(
-            shrinkWrap: true,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 150, 50, 0),
-                child: TextField(
-                  controller: _hnController,
-                  obscureText: false,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'หมายเลขผู้ป่วยนอก(HN)',
-                    errorText:
-                        _validateHN ? 'กรุณาใส่หมายเลขผู้ป่วยนอก(HN)' : null,
-                  ),
-                  inputFormatters: [UpperCaseTxt()],
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('AbdoCare'),
+      ),
+      body: Container(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(50, screenSize.height / 4, 50, 0),
+              child: TextField(
+                controller: _hnController,
+                obscureText: false,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'หมายเลขผู้ป่วยนอก(HN)',
+                  errorText:
+                      _validateHN ? 'กรุณาใส่หมายเลขผู้ป่วยนอก(HN)' : null,
+                ),
+                inputFormatters: [UpperCaseTxt()],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
+              child: TextField(
+                controller: _uniqueKeycontroller,
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'รหัสโค้ด',
+                  errorText:
+                      _validateUniqueKey ? 'กรุณาใส่รหัสโค้ด 6 หลัก' : null,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 20, 50, 0),
-                child: TextField(
-                  controller: _uniqueKeycontroller,
-                  obscureText: true,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'รหัสโค้ด',
-                    errorText:
-                        _validateUniqueKey ? 'กรุณาใส่รหัสโค้ด 6 หลัก' : null,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 10, 50, 100),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    RaisedButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(7.0)),
-                      color: Color(0xFF2ED47A),
-                      child: Text('ลงทะเบียน',
-                          style: TextStyle(fontSize: 18, color: Colors.white)),
-                      onPressed: () async {
-                        setState(() {
-                          _uniqueKeycontroller.text.isEmpty ||
-                                  _uniqueKeycontroller.text.length != 6
-                              ? _validateUniqueKey = true
-                              : _validateUniqueKey = false;
-
-                          _hnController.text.isEmpty ||
-                                  _hnController.text.length !=
-                                      7 // TODO Define HN length.
-                              ? _validateHN = true
-                              : _validateHN = false;
-                        });
-                        if (_validateHN == false &&
-                            _validateUniqueKey == false) {
-                          if (await _firebaseService.signIn(
-                              hn: _hnController.text.trim(),
-                              uniqueKey: _uniqueKeycontroller.text.trim())) {
-                            print("login success!");
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SetPinPage()),
-                            );
-                          }
-                        } else {
-                          print('login failed');
-                          _showErrorSignInDialog();
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              Column(
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(50, 10, 50, screenSize.height / 4),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset(
-                        'assets/Hospital_logo.png',
-                        height: 100,
-                        width: 80,
-                      ),
-                      Image.asset(
-                        'assets/STIN_logo.png',
-                        height: 100,
-                        width: 80,
-                      )
-                    ],
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7.0)),
+                    color: Color(0xFF2ED47A),
+                    child: Text('ลงทะเบียน',
+                        style: TextStyle(fontSize: 18, color: Colors.white)),
+                    onPressed: () async {
+                      setState(() {
+                        _uniqueKeycontroller.text.isEmpty ||
+                                _uniqueKeycontroller.text.length != 6
+                            ? _validateUniqueKey = true
+                            : _validateUniqueKey = false;
+
+                        _hnController.text.isEmpty ||
+                                _hnController.text.length !=
+                                    7 // TODO Define HN length.
+                            ? _validateHN = true
+                            : _validateHN = false;
+                      });
+                      if (_validateHN == false && _validateUniqueKey == false) {
+                        if (await _firebaseService.signIn(
+                            hn: _hnController.text.trim(),
+                            uniqueKey: _uniqueKeycontroller.text.trim())) {
+                          print("login success!");
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SetPinPage()),
+                          );
+                        }
+                      } else {
+                        print('login failed');
+                        _showErrorSignInDialog();
+                      }
+                    },
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/Hospital_logo.png',
+                      height: 100,
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/STIN_logo.png',
+                      height: 100,
+                      width: 80,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 
   void _showErrorSignInDialog() {
     var alertStyle = AlertStyle(
