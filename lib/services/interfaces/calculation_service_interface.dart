340342dde18e167ba3ae5e5c90978db662e61b28
PrompimonPang <papangdroppaul@gmail.com>
2021-02-03 19:36:39 +0700

    resolve conflict

--- lib/services/interfaces/calculation_service_interface.dart
+++ lib/services/interfaces/calculation_service_interface.dart
@@ -4,7 +4,6 @@ abstract class ICalculationService {
   int calculateAge(DateTime birthDate);
   String calculateBMI(int weight, int height);
   DateTime formatDate({DateTime date, String dateString});
-  String formatDateToThaiString({DateTime date, String dateString});
   String calculateBML(int oldWeight, int weight);
   String formatDateToThaiString(
       {DateTime date, String dateString, @required bool isBuddhist});
