03c156bf6dc035423f1a627e986bc6c8c94f9303
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2021-02-28 16:00:52 +0700

    Message widget

---
+++ lib/widget/chatbot/message.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Message extends StatelessWidget {
+  Message({this.text, this.name, this.type});
+
+  final String text;
+  final String name;
+  final bool type;
+
+  List<Widget> otherMessage(dynamic context) {
+    return <Widget>[
+      Container(
+        child: CircleAvatar(
+          child: Image.asset("assets/robot.jpg"),
+          backgroundColor: Colors.transparent,
+        ),
+      ),
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                constraints: BoxConstraints(maxWidth: 250),
+                decoration: BoxDecoration(
+                    color: Color(0xFFF1B43F),
+                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> myMessage(dynamic context) {
+    return <Widget>[
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Container(
+              constraints: BoxConstraints(maxWidth: 250),
+              decoration: BoxDecoration(
+                  color: Color(0xFFFCECBC),
+                  borderRadius: BorderRadius.all(Radius.circular(15.0))),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 5.0),
+        child: CircleAvatar(
+          backgroundImage: AssetImage("assets/default.jpg"),
+        ),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.type ? myMessage(context) : otherMessage(context),
+      ),
+    );
+  }
+}
