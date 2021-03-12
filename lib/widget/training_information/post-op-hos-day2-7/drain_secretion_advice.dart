7da6952688ebe3c6749544c8efc309e546ef5c53
Pormpimon <papangdroppaul@gmail.com>
2021-03-12 14:05:02 +0700

    fix bug

--- lib/widget/training_information/post-op-hos-day2-7/drain_secretion_advice.dart
+++ lib/widget/training_information/post-op-hos-day2-7/drain_secretion_advice.dart
@@ -19,8 +19,6 @@ class DrainSecretionAdviceDay2 extends StatelessWidget {
           onPressed: () {
             if (navigate == "Evaluate") {
               Navigator.pushNamed(context, '/evaluation_page');
-            } else if (navigate == "Chat") {
-              Navigator.pushNamed(context, '/chat_page');
             } else {
               Navigator.pop(context);
             }
