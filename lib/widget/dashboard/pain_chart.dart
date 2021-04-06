ea002a64049e53b343360cbba356eb7b149a9956
Pormpimon <papangdroppaul@gmail.com>
2021-04-06 16:32:01 +0700

    pain chart

--- lib/widget/dashboard/pain_chart.dart
+++ lib/widget/dashboard/pain_chart.dart
@@ -41,40 +41,72 @@ class PainChartState extends State<PainChart> {
     _PainData('14/03/64\n18.00น', 4),
     _PainData('14/03/64\n22.00น', 4),
   ];
+  ZoomPanBehavior _zoomPanBehavior;
+  @override
+  void initState() {
+    _zoomPanBehavior = ZoomPanBehavior(
+      enablePinching: true,
+      zoomMode: ZoomMode.x,
+      enablePanning: true,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
 
     return LayoutBuilder(builder: (context, constraints) {
       return SafeArea(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                  minWidth: constraints.minWidth,
-                  minHeight: constraints.minHeight),
-              child: IntrinsicWidth(
-                child: Container(
-                  height: screenSize.height / 2,
-                  width: screenSize.width * 4,
-                  child: SfCartesianChart(
-                      primaryXAxis: CategoryAxis(),
-                      tooltipBehavior: TooltipBehavior(enable: true),
-                      series: <ChartSeries<_PainData, String>>[
-                        LineSeries<_PainData, String>(
-                            dataSource: data,
-                            xValueMapper: (pain, _) => pain.painscore,
-                            yValueMapper: (pain, _) => pain.day,
-                            name: 'Pain score',
-                            color: Colors.red,
-                            dataLabelSettings:
-                                DataLabelSettings(isVisible: true))
-                      ]),
+        child: Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                      minWidth: constraints.minWidth,
+                      minHeight: constraints.minHeight),
+                  child: IntrinsicWidth(
+                    child: Container(
+                      height: screenSize.height / 2,
+                      width: screenSize.width * 4,
+                      child: SfCartesianChart(
+                          // zoomPanBehavior: _zoomPanBehavior,
+                          primaryXAxis: CategoryAxis(),
+                          primaryYAxis:
+                              NumericAxis(anchorRangeToVisiblePoints: true),
+                          tooltipBehavior: TooltipBehavior(enable: true),
+                          series: <ChartSeries<_PainData, String>>[
+                            LineSeries<_PainData, String>(
+                                dataSource: data,
+                                xValueMapper: (pain, _) => pain.painscore,
+                                yValueMapper: (pain, _) => pain.day,
+                                name: 'Pain score',
+                                color: Colors.red,
+                                dataLabelSettings:
+                                    DataLabelSettings(isVisible: true))
+                          ]),
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+            SfCartesianChart(
+                zoomPanBehavior: _zoomPanBehavior,
+                primaryXAxis: CategoryAxis(),
+                primaryYAxis: NumericAxis(anchorRangeToVisiblePoints: true),
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_PainData, String>>[
+                  LineSeries<_PainData, String>(
+                      dataSource: data,
+                      xValueMapper: (pain, _) => pain.painscore,
+                      yValueMapper: (pain, _) => pain.day,
+                      name: 'Pain score',
+                      color: Colors.red,
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ])
+          ],
         ),
       );
     });
