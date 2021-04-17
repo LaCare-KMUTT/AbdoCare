ce5dc4b165c80ddab9b6af0a6cfd4c2f9b3c9ebb
Pormpimon <papangdroppaul@gmail.com>
2021-04-17 17:27:21 +0700

    create reusable alert

--- lib/widget/shared/alert_style.dart
+++ lib/widget/shared/alert_style.dart
@@ -17,3 +17,56 @@ AlertStyle alertStyle = AlertStyle(
   ),
   alertAlignment: Alignment.center,
 );
+
+class Dialogs {
+  static Future<void> alertDialog(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            title: Text("กรุณาทำแบบประเมินให้ครบถ้วน",
+                style: Theme.of(context).textTheme.bodyText2,
+                textAlign: TextAlign.center),
+            actions: [
+              FlatButton(
+                color: Color(0xFFC37447),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: Text(
+                      "ตกลง",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ]);
+      },
+    );
+  }
+
+  static Future<void> alertDialogSaveData(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        Future.delayed(Duration(seconds: 1), () {});
+        return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            title: Icon(
+              Icons.check_circle_outline_rounded,
+              size: 100,
+              color: Colors.greenAccent,
+            ),
+            content: Text("บันทึกข้อมูลสำเร็จ",
+                style: Theme.of(context).textTheme.bodyText2,
+                textAlign: TextAlign.center));
+      },
+    );
+  }
+}
