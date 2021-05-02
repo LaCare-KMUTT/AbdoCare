d6d2a23fbb90b732f5e2d7f346102f54b696127b
Pormpimon <papangdroppaul@gmail.com>
2021-05-03 00:22:40 +0700

    fix notification UI

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import '../models/notification_list_model.dart';
+import '../services/service_locator.dart';
+import '../view_models/notification_view_model.dart';
 import '../widget/notification_card.dart';
+import '../widget/shared/loading_widget.dart';
 
 class NotificationPage extends StatefulWidget {
   @override
@@ -7,6 +11,19 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
+  final NotiViewModel _notiViewModel = locator<NotiViewModel>();
+
+  Future<List<NotiData>> _notidata;
+  List<NotiData> notidata = [];
+
+  @override
+  void initState() {
+    setState(() {
+      _notidata = _notiViewModel.getUsers();
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +40,24 @@ class _NotificationPageState extends State<NotificationPage> {
           },
         ),
       ),
-      body: NotificationCard(),
+      body: notificationDataBody(),
     );
+  }
+
+  FutureBuilder notificationDataBody() {
+    return FutureBuilder<List<NotiData>>(
+        future: _notidata,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return loadingProgress;
+          } else {
+            notidata.clear();
+            notidata.addAll(snapshot.data);
+
+            return NotificationCard(
+              notiData: notidata,
+            );
+          }
+        });
   }
 }
--- lib/widget/notification_card.dart
+++ lib/widget/notification_card.dart
@@ -1,155 +1,98 @@
-import 'package:AbdoCare/models/formname_model.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import '../models/formname_model.dart';
 import '../models/notification_list_model.dart';
-import '../widget/shared/loading_widget.dart';
 
 class NotificationCard extends StatefulWidget {
+  final List<NotiData> notiData;
+  NotificationCard({Key key, @required this.notiData}) : super(key: key);
   @override
   State<StatefulWidget> createState() => _NotificationCardState();
 }
 
 class _NotificationCardState extends State<NotificationCard> {
-  NotificationList _notificationList;
-  __initData() async {
-    var notificationList = await NotificationList.create();
-    setState(() {
-      _notificationList = notificationList;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    __initData();
-  }
-
-  String datetime(dynamic time, String value) {
-    var formTime =
-        DateTime.fromMicrosecondsSinceEpoch(time.microsecondsSinceEpoch);
-    var formDateToShow = DateFormat('dd/MM/yyyy').format(formTime);
-    var formTimeToShow = DateFormat.Hm().format(formTime).toString() + " น.";
-    if (value == "Date") {
-      return formDateToShow;
-    } else {
-      return formTimeToShow;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (_notificationList == null) {
-      return loadingProgress;
-    } else {
-      return FutureBuilder(
-          future: _notificationList.getNotificationList(),
-          builder: (context, _notification) {
-            if (!_notification.hasData) {
-              return loadingProgress;
-            }
-            return ListView(
-              shrinkWrap: true,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                  child: Column(
-                    children: <Widget>[
-                      for (var notification in _notification.data)
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: AbsorbPointer(
-                            absorbing: false,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ),
-                                shadowColor: Colors.black,
-                                side: BorderSide(
-                                    color: Colors.grey[200], width: 1),
-                              ),
-                              onPressed: () {
-                                if (notification['formName'] ==
-                                    "Surgical Incision") {
-                                  alertShowAdvice(
-                                      context,
-                                      notification['imgURL'],
-                                      notification['advice'],
-                                      notification['formName']);
-                                } else {
-                                  alertShowTraining(
-                                      context, notification['formName']);
-                                }
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.only(top: 10),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Text(
-                                          '''${datetime(notification['creation'], "Date")}\t\t${datetime(notification['creation'], "Time")}''',
-                                          style: TextStyle(
-                                            fontSize: 12.0,
-                                            color: Colors.grey[600],
-                                          ),
-                                          textAlign: TextAlign.right,
-                                        ),
-                                      ],
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 20),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            '''${formNameModel[notification['formName']]}''',
-                                            style: TextStyle(
-                                                fontSize: 16.0,
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          (() {
-                                            if (notification['formName'] ==
-                                                "Surgical Incision") {
-                                              return Text(
-                                                '''บุคลากรทางการแพทย์ได้ส่งคำแนะนำถึงคุณ''',
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .bodyText1,
-                                              );
-                                            } else {
-                                              return Text(
-                                                '''ผู้ป่วยไม่ผ่านแบบประเมิน''',
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .bodyText1,
-                                              );
-                                            }
-                                          }()),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                    ],
-                  ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+      child: Column(
+          children: widget.notiData.map((noti) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: AbsorbPointer(
+            absorbing: false,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-              ],
-            );
-          });
-    }
+                shadowColor: Colors.black,
+                side: BorderSide(color: Colors.grey[200], width: 1),
+              ),
+              onPressed: () {
+                if (noti.formName == "แบบประเมินแผล") {
+                  alertShowAdvice(
+                      context, noti.imgURL, noti.advice, noti.formName);
+                } else {
+                  alertShowTraining(context, noti.formName);
+                }
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          '''${noti.formDate}\t\t${noti.formTime}''',
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.grey[600],
+                          ),
+                          textAlign: TextAlign.right,
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '''${noti.formName}''',
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          (() {
+                            if (noti.formName == "แบบประเมินแผล") {
+                              return Text(
+                                '''บุคลากรทางการแพทย์ได้ส่งคำแนะนำถึงคุณ''',
+                                style: Theme.of(context).textTheme.bodyText1,
+                              );
+                            } else {
+                              return Text(
+                                '''ผู้ป่วยไม่ผ่านแบบประเมิน''',
+                                style: Theme.of(context).textTheme.bodyText1,
+                              );
+                            }
+                          }()),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }).toList()),
+    );
   }
 
   Future<void> alertShowAdvice(BuildContext context, String imgURL,
@@ -176,7 +119,7 @@ class _NotificationCardState extends State<NotificationCard> {
                     Column(
                       children: [
                         Text(
-                          "ผู้ป่วยไม่ผ่าน${formNameModel[formName]}",
+                          "ผู้ป่วยไม่ผ่าน$formName",
                           textAlign: TextAlign.center,
                         ),
                         Text(
