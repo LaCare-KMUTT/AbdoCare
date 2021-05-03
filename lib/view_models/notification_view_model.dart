55774dac8e3bb8cfe6deaef628fc8aeb2eba0531
Pormpimon <papangdroppaul@gmail.com>
2021-05-03 20:42:05 +0700

    change variable name

--- lib/view_models/notification_view_model.dart
+++ lib/view_models/notification_view_model.dart
@@ -5,7 +5,7 @@ import '../services/service_locator.dart';
 class NotiViewModel {
   NotiViewModel();
   final _firebaseService = locator<IFirebaseService>();
-  List<NotiData> userList = [];
+  List<NotiData> notiList = [];
 
   Future<void> _initialize() async {
     var allNotiList = await _firebaseService.getNotifications();
@@ -14,33 +14,33 @@ class NotiViewModel {
     if (allNotiList != null) {
       // ignore: avoid_function_literals_in_foreach_calls
       allNotiList.forEach((mapData) {
-        userList.add(NotiData(map: mapData));
+        notiList.add(NotiData(map: mapData));
         sortBy("patientSeen", true);
       });
     }
   }
 
   Future<List<NotiData>> getUsers() async {
-    userList.clear();
+    notiList.clear();
     await _initialize();
     // ignore: avoid_function_literals_in_foreach_calls
-    userList.forEach((element) {});
-    return userList;
+    notiList.forEach((element) {});
+    return notiList;
   }
 
   // ignore: avoid_positional_boolean_parameters
   List<NotiData> sortBy(String key, bool isAsc) {
     switch (key) {
       case 'patientSeen':
-        userList
+        notiList
             .sort((a, b) => a.formDateTimeSort.compareTo(b.formDateTimeSort));
-        userList.sort((a, b) => b.patientSeen.compareTo(a.patientSeen));
+        notiList.sort((a, b) => b.patientSeen.compareTo(a.patientSeen));
         break;
     }
     if (isAsc) {
-      userList = userList.reversed.toList();
+      notiList = notiList.reversed.toList();
     }
 
-    return userList;
+    return notiList;
   }
 }
